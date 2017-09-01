af441fe08b2adb50cabdeee38c34f35da3c0e3fb
mlabouardy <mohamed@labouardy.com>
2017-09-01 14:34:46 +0200

    clone respo

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,6 @@ yum update
 yum install -y golang
 mkdir -p /home/ec2-user/go/src/github.com/
 cd /home/ec2-user/go/src/github.com/
-git clone
+git clone https://github.com/mlabouardy/rds-go
+cd rds-go
 go get ./...
